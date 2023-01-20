3d5984a0788f2acd43844bd6a3e75d46c41b968f
Abdullah Nasser <abdullah-nasser-@outlook.com>
2023-01-20 19:45:05 +0300

    FtpConnection module formatted the file fixed typos and replaced ' with " when necessary.

--- week-1/project-1/FtpConnection.py
+++ week-1/project-1/FtpConnection.py
@@ -7,34 +7,33 @@ from ftplib import FTP, error_perm  # importing library to connect to the server
 from schedule import every, run_pending
 
 from dotenv import load_dotenv  # importing method to load config data
-from configparser import ConfigParser  # importing file to read configurating params
+from configparser import ConfigParser  # importing file to read configuration params
 
 load_dotenv()  # loading config data
 try:  # trying to load settings from config.ini
     local_settings = ConfigParser()
     local_settings.read("config.ini")
-    path_to_download = local_settings['local-path']['local-path']
-    path_to_move = local_settings['local-path']['moving-path']
-    scheduled_time = local_settings['local-path']['run-time'] # o start execution current_time < scheduled_time
-except KeyError:   # if the file wasn't found we put default settings
-    path_to_download = './'
+    path_to_download = local_settings["local-path"]["local-path"]
+    path_to_move = local_settings["local-path"]["moving-path"]
+    scheduled_time = local_settings["schedule"]["run-time"]  # o start execution current_time < scheduled_time
+except KeyError:  # if the file wasn't found we put default settings
+    path_to_download = "./"
     path_to_move = "./moving-folder/"
     scheduled_time = "9:00"  # to start execution current_time < scheduled_time
 
 # loading config data from .env file to connect to ftp server.
-# we assume that the server is secured and has authentification
+# we assume that the server is secured and has authentication
 host = os.environ["host"]
 user = os.environ["user"]
 psw = os.environ["psw"]
 
-
-# specify config for logging, logs go to the terminal and to the file with the name DATE_OF_RUNNING_THE_SCRTIPT.log
+# specify config for logging, logs go to the terminal and to the file with the name DATE_OF_RUNNING_THE_SCRIPT.log
 # for example 20.01.2023.log
-logging.basicConfig(format='%(asctime)s-%(levelname)s - %(message)s',
+logging.basicConfig(format="%(asctime)s-%(levelname)s - %(message)s",
                     level=logging.INFO,
-                    datefmt='%d.%m.%Y %H:%M:%S',
+                    datefmt="%d.%m.%Y %H:%M:%S",
                     handlers=[
-                        logging.FileHandler(filename=f'{datetime.datetime.now().date().strftime("%d.%m.%Y")}.log'),
+                        logging.FileHandler(filename=f"{datetime.datetime.now().date().strftime('%d.%m.%Y')}.log"),
                         logging.StreamHandler()
                     ])
 
@@ -48,24 +47,24 @@ def download_data_from_ftp_server(host: str, usr: str, psw: str, download_path: 
     :param download_path:
     :return:
     """
-    if not host or not download_path:  # raise ValueError if not enought params
-        raise ValueError('Plese provide all params')
+    if not host or not download_path:  # raise ValueError if not enough params
+        raise ValueError("Please provide all params")
     if not os.path.exists(download_path):  # if local path for download wasn't found, we create a folder
         os.mkdir(download_path)
 
-    with FTP(host) as ftp:  # oppening connection to ftp server using context manager with
-        try:  # try to login to server
+    with FTP(host) as ftp:  # opening connection to ftp server using context manager with
+        try:  # try to log in to server
             ftp.login(user=usr, passwd=psw)
-            logging.info(f'succesful login host {usr}@{host}')
+            logging.info(f"successful login host {usr}@{host}")
         except error_perm:  # raise an error if credentials are incorrect
-            logging.error(f'Incorrect login or password')
+            logging.error(f"Incorrect login or password")
             raise ValueError("Incorrect login or password")
         files = ftp.nlst()  # get names of files in current directory of ftp server
-        logging.info(f'downloaded from {host} files: {files}')
+        logging.info(f"downloaded from {host} files: {files}")
         for file in files:  # downloading files to the specified location
-            with open(f'{path_to_download}{"/" if path_to_download[-1] != "/" else ""}{file}', 'wb') as fp:
-                ftp.retrbinary(f'RETR {file}', fp.write)
-        logging.info(f'downloading from {host} finished')
+            with open(f"{path_to_download}{'/' if path_to_download[-1] != '/' else ''}{file}", "wb") as fp:
+                ftp.retrbinary(f"RETR {file}", fp.write)
+        logging.info(f"downloading from {host} finished")
 
 
 def move_downloaded(src: str, dist: str) -> None:
@@ -82,14 +81,14 @@ def move_downloaded(src: str, dist: str) -> None:
         raise FileNotFoundError("The source file/folder doesn't exists, make sure you entered the correct path.")
 
     try:
-        logging.info(f'starting transfering from {src} to {dist}')
+        logging.info(f"starting transferring from {src} to {dist}")
         if not os.path.exists(dist):  # creating a destination directory if the one does not exist
             os.mkdir(dist)
         shutil.copytree(src=src, dst=dist, dirs_exist_ok=True)  # method copies data from source path to dest,
         # overriding existing files
-        logging.info(f'transfer finished')
+        logging.info(f"transfer finished")
     except shutil.Error as e:
-        logging.error(f'shutil.Error {e}', stack_info=True)
+        logging.error(f"shutil.Error {e}", stack_info=True)
 
 
 def work(host: str, usr: str, psw: str, download_path: str, dist: str) -> None:
@@ -97,9 +96,9 @@ def work(host: str, usr: str, psw: str, download_path: str, dist: str) -> None:
     Function to put all actions in one place.
     Firstly downloads data from FTP and then moves it to destination path
     """
-    logging.info('starting working process')
+    logging.info("starting working process")
     if not host or not download_path or not download_path or not dist:
-        raise ValueError('Plese provide all params')
+        raise ValueError("Please provide all params")
     try:
         download_data_from_ftp_server(host, usr, psw, download_path)
     except Exception as e:
@@ -110,20 +109,20 @@ def work(host: str, usr: str, psw: str, download_path: str, dist: str) -> None:
     except Exception as e:
         logging.error(e, stack_info=True)
         exit()
-    logging.info('finished working proccess')
+    logging.info("finished working process")
 
 
 def main():
     """Main function. Specifies and runs the scheduler"""
-    logging.info('start main')
+    logging.info("start main")
     every().day.at(scheduled_time).do(work, host, user, psw, path_to_download, path_to_move)
     while 1:
         run_pending()
         time.sleep(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()  # call the main function and start execution process
     except KeyboardInterrupt:
-        logging.info('program finished by user')
+        logging.info("program finished by user")
