f3064e82f822ac4abc99827a47c33d370eb5b5e4
efpcode <efpjobhunt@gmail.com>
2023-01-21 15:47:00 +0100

    Renaming class minor change

--- week-1/project-3/src/db_transactions.py
+++ week-1/project-3/src/db_transactions.py
@@ -1,8 +1,8 @@
 from pathlib import Path
+from sqlite3 import connect as sql_connect
 
 
-class DataBaseTransaction:
-
+class DataBaseManager:
     def create_db(self, db_name: str = None):
         if not db_name:
             db_name = "db_transaction"
@@ -17,3 +17,26 @@ class DataBaseTransaction:
             db_path.touch()
         else:
             print("DB already exists")
+
+    def db_connect(self, path_to_db: str):
+        db_path = Path() / path_to_db
+        try:
+            if not db_path.exists():
+                raise FileNotFoundError
+
+        except (TypeError, FileNotFoundError) as e:
+            print(f"DB to path has not been passed correctly:\n\n {e}")
+        else:
+            conn = sql_connect(db_path)
+            return conn
+
+    def db_close(self, connector, cursor):
+        try:
+            cursor.close()
+            connector.close()
+
+        except AttributeError:
+            print("Cursor and Connector are still open.")
+
+        else:
+            print("All DB connection have been terminated")
--- week-1/project-3/tests/db_transactions_test.py
+++ week-1/project-3/tests/db_transactions_test.py
@@ -1,22 +1,35 @@
 import pytest
-from src.db_transactions import DataBaseTransaction
+from src.db_transactions import DataBaseManager
 from pathlib import Path
+
 
 @pytest.fixture
 def create_db_object():
-    db_transact = DataBaseTransaction()
+    db_transact = DataBaseManager()
     return db_transact
 
 
-def test_db_creation(create_db_object):
+@pytest.fixture()
+def create_db_file(create_db_object):
+    create_db_object.create_db("tmp")
+    return create_db_object
+
+
+def test_db_creation(create_db_file):
     file_obj = Path().cwd()
-    expected_file = file_obj / "db_transaction.db"
-    create_db_object.create_db()
-    file_found = [
-        db_file for db_file in file_obj.iterdir() if db_file == expected_file
-    ]
+    expected_file = file_obj / "tmp.db"
+    file_found = [db_file for db_file in file_obj.iterdir() if db_file == expected_file]
     assert expected_file == file_found[0]
 
 
+@pytest.mark.xfail(raises=FileNotFoundError)
+def test_db_connect_none_file(create_db_file):
+    conn = create_db_file.db_connect("tester.db")
 
 
+def test_db_connection(create_db_file, capsys):
+    conn = create_db_file.db_connect("tmp.db")
+    cursor = conn.cursor()
+    create_db_file.db_close(conn, cursor)
+    captured_msg = capsys.readouterr()
+    assert captured_msg.out == "All DB connection have been terminated\n"
