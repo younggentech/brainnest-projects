607e69ec2f23f581e2c98f990bc41719b66748f5
efpcode <efpjobhunt@gmail.com>
2023-01-21 13:10:50 +0100

    Added DB class Method create_db This method creates a regular sqlite3 db at current working dir (cwd)

--- week-1/project-3/src/db_transactions.py
+++ week-1/project-3/src/db_transactions.py
@@ -5,8 +5,8 @@ class DataBaseTransaction:
 
     def create_db(self, db_name: str = None):
         if not db_name:
-            db_name = "db_transaction.db"
-        db_path = Path().cwd() / db_name
+            db_name = "db_transaction"
+        db_path = Path().cwd() / f"{db_name}.db"
         try:
             if not db_path.exists():
                 raise FileNotFoundError
