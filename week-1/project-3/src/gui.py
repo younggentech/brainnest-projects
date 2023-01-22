f54fbe6611594cac42e88f0597d7983b1b86c7e1
efpcode <efpjobhunt@gmail.com>
2023-01-22 18:17:38 +0100

    Rename + Radiobutton Minor changes Rename all the variable to make it bit better more understandable. * Left to do is: send values db, validiator that can be done with   private method use def _validator + regex library recommendation   to download re and update requirements text..

--- week-1/project-3/src/gui.py
+++ week-1/project-3/src/gui.py
@@ -4,35 +4,59 @@ from datetime import datetime
 
 class WindowBudget:
     def __init__(self, budget_win: object):
-        self.lbl1 = Label(budget_win, text="Amount")
-        self.lbl2 = Label(budget_win, text="Habits")
-        self.lbl3 = Label(budget_win, text="Total Budget")
-        self.txtfd1 = Entry()
-        self.txtfd2 = Entry()
-        self.txtfd3 = Entry()
-        self.btn1 = Button(budget_win, text="Add new row")
-        self.b1 = Button(budget_win, text="Add new row", command=self.add_row)
+        self.lbl_amount = Label(budget_win, text="Amount")  # Amount labels
+        self.lbl_habits = Label(budget_win, text="Habits")  # Habits labels
+        self.lbl_goalbudget = Label(budget_win, text="Goal Budget")  # Goal labels
+        self.lbl_outcome = Label(budget_win, text="empty")
+        self.lbl_income = Label(budget_win, text="empty")
+        self.txt_fd_amount = Entry()  # Amount input field
+        self.txt_fd_habits = Entry()  # input field habits
+        self.txt_fd_goalbudget = Entry()  # see comments above
+        self.btn1_trx = Button(budget_win, text="Add new transaction")
+        self.b1_trx = Button(budget_win, text="Add new transaction",
+                             command=self.add_new_trx)
 
-        self.btn3 = Button(budget_win, text="Update Total Budget")
-        self.b3 = Button(budget_win, text="Update Total Budget",
-                         command=self.total_budget)
-        self.lbl1.place(x=238, y=180)
-        self.txtfd1.place(x=175, y=150)
-        self.b1.place(x=50, y=150)
-        self.lbl2.place(x=438, y=180)
-        self.txtfd2.place(x=375, y=150)
+        self.radiobtn_selector = StringVar(None, "income")
 
-        self.lbl3.place(x=50, y=50)
-        self.txtfd3.place(x=215, y=50)
-        self.b3.place(x=50, y=50)
+        self.btn5_outcome = Radiobutton(budget_win, text="outcome",
+                                        variable=self.radiobtn_selector,
+                                        value="outcome")
+        self.btn7_income = Radiobutton(budget_win, text="income",
+                                       command=self.radio_button,
+                                       variable=self.radiobtn_selector,
+                                       value="income")
 
-    def add_row(self):
+        self.btn3_goalbudget = Button(budget_win, text="Update Goal Budget")
+        self.b3_goalbudget = Button(budget_win, text="Update Goal Budget",
+                                    command=self.update_goal_budget)
+
+        self.lbl_amount.place(x=282, y=180)
+        self.txt_fd_amount.place(x=220, y=150)
+        self.b1_trx.place(x=50, y=150)
+        self.lbl_habits.place(x=520, y=180)
+        self.txt_fd_habits.place(x=438, y=150)
+
+        self.lbl_goalbudget.place(x=50, y=50)
+        self.txt_fd_goalbudget.place(x=215, y=50)
+        self.b3_goalbudget.place(x=50, y=50)
+
+        self.btn5_outcome.place(x=650, y=175)
+        self.btn7_income.place(x=650, y=150)
+
+    def add_new_trx(self):
         timestamp = datetime.now()
-        amount = self.txtfd1.get()
-        habits = self.txtfd2.get()
+        amount = self.txt_fd_amount.get()
+        habits = self.txt_fd_habits.get()
         print(timestamp, amount, habits)
         # todo: here we can save the row
 
-    def total_budget(self):
-        total_budget = self.txtfd3.get()
-        print(total_budget)
+    def update_goal_budget(self):
+        total_budget = self.txt_fd_goalbudget.get()
+        timestamp = datetime.now()
+        print(total_budget, timestamp)
+
+    def radio_button(self):
+        print(self.radiobtn_selector.get())
+        # TODO: (Esteban) - initial set values is not gathered
+        #   Method returns None or income.
+
