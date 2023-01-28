95eea7ae53239d5059dc75dba071d4e924722f7e
Alexandr Ovsyannikov <alexov2004@gmail.com>
2023-01-29 03:10:48 +0400

    Hangman module almost finished, using TDD

--- week2/project-1/backend/src/hangman/HangmanGame.py
+++ week2/project-1/backend/src/hangman/HangmanGame.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from typing import Union
 
 
@@ -15,14 +16,50 @@ class AbstractHangman(ABC):
 
 
 class Hangman(AbstractHangman):
-    def __init__(self, words_details):
+    def __init__(self, words_details: dict, attempts: int):
         self.words_details = words_details
+        self.__attempts = attempts
+        self.__guessed = {
+            "letters": set(),
+            "letter_to_position": defaultdict(lambda: []),
+        }
 
     def finish_game(self) -> Union[bool, dict]:
-        pass
+        if len(self.__guessed["letters"]) == len(set(self.words_details["word"])):
+            return True, {"isWin": True, "isLoss": False}
+        elif self.__attempts == 0:
+            return True, {"isWin": False, "isLoss": True}
+        return False, {"isWin": False, "isLoss": False}
 
     def check_letter(self, letter: str) -> dict:
-        pass
+        if letter in self.words_details["word"]:
+            self.__guessed["letters"].add(letter)
+            self.__guessed["letter_to_position"][letter] = [
+                pos
+                for pos, char in enumerate(self.words_details["word"])
+                if char == letter
+            ]
+        else:
+            self.__attempts -= 1
+        resp = {
+            "letter": letter,
+            "positions": self.__guessed["letter_to_position"][letter],
+            "attemptsLeft": self.__attempts,
+        }
+        return resp | self.finish_game()[1]
+
+    def get_current_letter_state(self) -> dict:
+        return self.__guessed["letter_to_position"]
+
+    @property
+    def attempts(self):
+        return self.__attempts
+
+    @attempts.setter
+    def set_attempts(self, attempts: int):
+        if not isinstance(attempts, int):
+            raise TypeError("attempts should be of class int")
+        self.__attempts = attempts
 
     @property
     def words_details(self):
@@ -32,11 +69,15 @@ class Hangman(AbstractHangman):
     def words_details(self, word_data: dict):
         if not isinstance(word_data, dict):
             raise TypeError("word_data should be dict")
-        if not word_data.get("definitions"):
-            raise ValueError("definitions should exist")
+        if not word_data.get("word") or (
+            word_data.get("word") and not isinstance(word_data.get("word"), str)
+        ):
+            raise ValueError("word key is essential, the value should be str")
         self.__words_details = word_data
 
 
 if __name__ == "__main__":
-    h = Hangman("ads")
-    print(h.words_details)
+    h = Hangman({"word": "hello"}, 6)
+    print(h.check_letter("p"))
+    print(h.check_letter("e"))
+    print(h.get_current_letter_state())
--- week2/project-1/backend/src/hangman/tests.py
+++ week2/project-1/backend/src/hangman/tests.py
@@ -1,6 +1,63 @@
 import unittest
 from . import Hangman
 
+CORRECT_DATA = {
+    "id": 3,
+    "word": "aardvark",
+    "definitions": [
+        {
+            "word": "aardvark",
+            "phonetic": "/ˈɑːd.vɑːk/",
+            "phonetics": [
+                {"text": "/ˈɑːd.vɑːk/", "audio": ""},
+                {
+                    "text": "/ˈɑɹd.vɑɹk/",
+                    "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/aardvark-us.mp3",
+                    "sourceUrl": "https://commons.wikimedia.org/w/index.php?curid=50309155",
+                    "license": {
+                        "name": "BY-SA 4.0",
+                        "url": "https://creativecommons.org/licenses/by-sa/4.0",
+                    },
+                },
+            ],
+            "meanings": [
+                {
+                    "partOfSpeech": "noun",
+                    "definitions": [
+                        {
+                            "definition": "The nocturnal, insectivorous, burrowing, mammal Orycteropus afer, of the order Tubulidentata, somewhat resembling a pig, common in some parts of sub-Saharan Africa.",
+                            "synonyms": [],
+                            "antonyms": [],
+                            "example": "The aardvark burrows in the ground and feeds mostly on termites, which it catches with its long, slimy tongue.",
+                        },
+                        {
+                            "definition": "A silly or credulous person who is prone to mistakes or blunders.",
+                            "synonyms": [],
+                            "antonyms": [],
+                            "example": "I walked into the wrong bathroom like a total aardvark.",
+                        },
+                    ],
+                    "synonyms": [
+                        "African anteater",
+                        "ant bear",
+                        "ant-bear",
+                        "antbear",
+                        "anteater",
+                        "earth pig",
+                        "fool",
+                    ],
+                    "antonyms": [],
+                }
+            ],
+            "license": {
+                "name": "CC BY-SA 3.0",
+                "url": "https://creativecommons.org/licenses/by-sa/3.0",
+            },
+            "sourceUrls": ["https://en.wiktionary.org/wiki/aardvark"],
+        }
+    ],
+}
+
 
 class HangmanTest(unittest.TestCase):
     def test_init_string(self):
@@ -8,71 +65,144 @@ class HangmanTest(unittest.TestCase):
         with self.assertRaises(TypeError):
             Hangman("Word")
 
-    def test_json_structure(self):
-        """Initialise the Hangman object with string value"""
-        word_data = {"id": 3, "word": "aardvark"}
-        with self.assertRaises(ValueError):
-            Hangman(word_data)
-
     def test_init(self):
+        """Testing initiating of Hangman object"""
+        # word aardvark
         """Try to initialize the object, put data there and get it back"""
-        word_data = {
-            "id": 3,
-            "word": "aardvark",
-            "definitions": [
-                {
-                    "word": "aardvark",
-                    "phonetic": "/ˈɑːd.vɑːk/",
-                    "phonetics": [
-                        {"text": "/ˈɑːd.vɑːk/", "audio": ""},
-                        {
-                            "text": "/ˈɑɹd.vɑɹk/",
-                            "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/aardvark-us.mp3",
-                            "sourceUrl": "https://commons.wikimedia.org/w/index.php?curid=50309155",
-                            "license": {
-                                "name": "BY-SA 4.0",
-                                "url": "https://creativecommons.org/licenses/by-sa/4.0",
-                            },
-                        },
-                    ],
-                    "meanings": [
-                        {
-                            "partOfSpeech": "noun",
-                            "definitions": [
-                                {
-                                    "definition": "The nocturnal, insectivorous, burrowing, mammal Orycteropus afer, of the order Tubulidentata, somewhat resembling a pig, common in some parts of sub-Saharan Africa.",
-                                    "synonyms": [],
-                                    "antonyms": [],
-                                    "example": "The aardvark burrows in the ground and feeds mostly on termites, which it catches with its long, slimy tongue.",
-                                },
-                                {
-                                    "definition": "A silly or credulous person who is prone to mistakes or blunders.",
-                                    "synonyms": [],
-                                    "antonyms": [],
-                                    "example": "I walked into the wrong bathroom like a total aardvark.",
-                                },
-                            ],
-                            "synonyms": [
-                                "African anteater",
-                                "ant bear",
-                                "ant-bear",
-                                "antbear",
-                                "anteater",
-                                "earth pig",
-                                "fool",
-                            ],
-                            "antonyms": [],
-                        }
-                    ],
-                    "license": {
-                        "name": "CC BY-SA 3.0",
-                        "url": "https://creativecommons.org/licenses/by-sa/3.0",
-                    },
-                    "sourceUrls": ["https://en.wiktionary.org/wiki/aardvark"],
-                }
-            ],
-        }
-        self.assertEqual(Hangman(word_data).words_details, word_data)
+        hgm = Hangman(CORRECT_DATA, 6)
+        self.assertEqual(hgm.words_details, CORRECT_DATA)
+        print(hgm.attempts)
+        self.assertEqual(hgm.attempts, 6)
+
+    def test_check_letters(self):
+        """Trying to play a game, winning with 1 mistake"""
+        hgm = Hangman(CORRECT_DATA, 6)
+
+        self.assertEqual(
+            hgm.check_letter("a"),
+            {
+                "letter": "a",
+                "positions": [0, 1, 5],
+                "attemptsLeft": 6,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("p"),
+            {
+                "letter": "p",
+                "positions": [],
+                "attemptsLeft": 5,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("r"),
+            {
+                "letter": "r",
+                "positions": [2, 6],
+                "attemptsLeft": 5,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("d"),
+            {
+                "letter": "d",
+                "positions": [3],
+                "attemptsLeft": 5,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("k"),
+            {
+                "letter": "k",
+                "positions": [7],
+                "attemptsLeft": 5,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("v"),
+            {
+                "letter": "v",
+                "positions": [4],
+                "attemptsLeft": 5,
+                "isWin": True,
+                "isLoss": False,
+            },
+        )
+
+    def test_check_loose(self):
+        """Trying to play a game, loosing"""
+        hgm = Hangman(CORRECT_DATA, 6)
+
+        self.assertEqual(
+            hgm.check_letter("z"),
+            {
+                "letter": "z",
+                "positions": [],
+                "attemptsLeft": 5,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("x"),
+            {
+                "letter": "x",
+                "positions": [],
+                "attemptsLeft": 4,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("y"),
+            {
+                "letter": "y",
+                "positions": [],
+                "attemptsLeft": 3,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("m"),
+            {
+                "letter": "m",
+                "positions": [],
+                "attemptsLeft": 2,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("q"),
+            {
+                "letter": "q",
+                "positions": [],
+                "attemptsLeft": 1,
+                "isWin": False,
+                "isLoss": False,
+            },
+        )
+        self.assertEqual(
+            hgm.check_letter("w"),
+            {
+                "letter": "w",
+                "positions": [],
+                "attemptsLeft": 0,
+                "isWin": False,
+                "isLoss": True,
+            },
+        )
 
 
 if __name__ == "__main__":
