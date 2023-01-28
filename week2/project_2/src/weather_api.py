afabac400d3edc43130d5f74ee45170565133f61
efpcode <efpjobhunt@gmail.com>
2023-01-28 14:52:35 +0100

    Added new structure + fuction minor change rename of files from __init__ to weather_app added function called get_weather_data in weather_api - should be refactor later on. no tests

--- week2/project_2/src/weather_api.py
+++ week2/project_2/src/weather_api.py
@@ -1,4 +1,28 @@
-from __init__ import app
+# -*- coding: utf-8 -*-
+""" This module was created to deal with response data from weather API
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+"""
+from requests import Response
+
+
+def get_weather_data(response_obj: Response) -> list:
+    """
+
+    :param response_obj:
+    :return:
+    """
+    weather_data = [
+        "name",
+        "country",
+        "localtime",
+        "temp_c",
+        "feelslike_c",
+        "humidity",
+        "condition",
+    ]
+    results = []
+    for key in response_obj:
+        for item in weather_data:
+            results.append(response_obj[key].get(item, None))
+
+    return (item for item in results if item)
